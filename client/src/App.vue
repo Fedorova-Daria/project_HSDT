d093e15b25b4384b60c70a6a7f1de8d8a399917a
Fedorova-Daria <fedorovada06@gmail.com>
2025-02-21 15:50:22 +0500

    Фронт

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  methods: {
+    goToHome() {
+      this.$router.push("/");
+    },
+    goToAbout() {
+      this.$router.push("/register");
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
---
+++ client/src/router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "./components/Login.vue"; // Импортируйте компонент Login
+import Register from "./components/Register.vue"; // Импортируйте компонент Register
+
+const routes = [
+  { path: "/", redirect: "/login" }, // Перенаправление с корневого маршрута на страницу входа
+  { path: "/login", component: Login }, // Маршрут для страницы входа
+  { path: "/register", component: Register }, // Маршрут для страницы регистрации
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
