8e083f60fa96f2de9f3c2dee65d18d6c9e054b6e
Fedorova-Daria <fedorovada06@gmail.com>
2025-03-15 14:59:48 +0500

    и тут(

--- client/src/components/Ideas.vue
+++ client/src/components/Ideas.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
     <Header />
+    <table
+      class="w-3/5 m-auto border-collapse shadow-lg rounded-lg overflow-hidden"
+    >
+      <thead class="bg-zinc-800 text-white">
+        <tr>
+          <th class="p-3 text-left">Название идеи</th>
+          <th class="p-3 text-left">Статус</th>
+          <th class="p-3 text-left">Дата создания</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="bg-cards transition-colors text-white hover:bg-zinc-700">
+          <td class="p-3 border-t border-zinc-600"></td>
+          <td class="p-3 border-t border-zinc-600"></td>
+          <td class="p-3 border-t border-zinc-600"></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
