a715f3310ca02663c47a8e45326a1b5865083530
Uhalov Arseny <akukhalov@gmail.com>
2025-03-25 22:19:21 +0500

    Другая сперма(хз)

--- server/projects/serializers.py
+++ server/projects/serializers.py
@@ -22,10 +22,26 @@ class ProjectListSerializer(serializers.ModelSerializer):
     likes_count = serializers.IntegerField(source="likes.count", read_only=True)  # Количество лайков
     applicants_count = serializers.IntegerField(source="applicants.count", read_only=True)  # Количество откликнувшихся
     workers = AccountSerializer(many=True, read_only=True)  # Проголосовавшие эксперты (или работающие пользователи)
+    is_liked = serializers.SerializerMethodField()
+    is_expert_voted = serializers.SerializerMethodField()
+    experts_voted_count = serializers.IntegerField(source="experts_voted.count", read_only=True)
+
 
     class Meta:
         model = Project
         fields = "__all__"  # Включаем все поля из модели + добавленные через сериализаторы
+        extra_kwargs = {
+            'experts_voted': {'write_only': True}  # Скрываем в ответе, так как используем is_expert_voted
+        }
+
+    def get_is_liked(self, obj):
+        user = self.context['request'].user
+        return user.is_authenticated and obj.likes.filter(id=user.id).exists()
+
+    def get_is_expert_voted(self, obj):
+        user = self.context['request'].user
+        return user.is_authenticated and hasattr(user, 'is_expert') and user.is_expert and obj.experts_voted.filter(id=user.id).exists()
+
 
 # Сериализатор для создания проекта
 class ProjectCreateSerializer(serializers.ModelSerializer):
