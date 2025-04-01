0b6ca271e31a4b7b68ce5f915b5038a52f10a710
KarandaAR <karandaalexandr@gmail.com>
2025-04-01 18:31:17 +0500

    база по командам и кое-что

---
+++ server/teams/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from users.models import Account
+
+class Team(models.Model):
+    STATUS_CHOICES = [
+        ("open", "Открыта"),
+        ("private", "Приватная"),
+        ("disbanded", "Расформирована"),
+    ]
+
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    owner = models.OneToOneField(Account, on_delete=models.CASCADE, related_name="owned_team")
+    members = models.ManyToManyField(Account, related_name="teams", blank=True)
+    avatar = models.ImageField(upload_to="team_avatars/", blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="open")
+
+    def __str__(self):
+        return self.name
+
